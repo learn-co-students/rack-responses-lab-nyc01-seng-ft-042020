49125ef0c2d4a43728bc95bce1aa8073e28ea44e
Srishti Nath <srishti.nath@gmail.com>
2020-05-10 23:47:44 -0400

    Done.

---
+++ app/application.rb
@@ -0,0 +1,17 @@
+class Application
+ 
+    def call(env)
+      resp = Rack::Response.new
+
+      t = Time.now
+   
+      if t.hour > 12
+        resp.write "Good afternoon!"
+      else
+        resp.write "Good morning!"
+      end
+   
+      resp.finish
+    end
+   
+  end
